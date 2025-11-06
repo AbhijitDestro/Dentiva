fe9af6567d7a60e8199147df761dbabee6d3328c
AbhijitDestro <abhijitdutta1806@gmail.com>
2025-11-06 22:15:28 +0530

    Added the schema and syncing user with the database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,6 @@ export default function RootLayout({
       >
         <html lang="en">
           <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
-            {/* this is done in the home page component */}
-            {/* <UserSync /> */}
             <Toaster />
             {children}
           </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,15 @@ import WhatToAsk from "@/components/Landing/WhatToAsk";
 import PricingSection from "@/components/Landing/PricingSection";
 import CTA from "@/components/Landing/CTA";
 import Footer from "@/components/Landing/Footer";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { syncUser } from "@/lib/actions/users";
 
-export default function Home() {
+export default async function Home() {
+  const user=await currentUser()
+   await syncUser();
+  if(user) redirect("/dashboard")
+
   return (
     <div className="min-h-screen bg-background">
       <Header />
