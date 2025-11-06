fccc9aeb5b38b15f9b8a770622fe3e567a716fd9
AbhijitDestro <abhijitdutta1806@gmail.com>
2025-11-06 23:34:26 +0530

    Custom Sign-in and Sign-up pages done and added the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import ActivityOverview from "@/components/Dashboard/ActivityOverview";
+import MainActions from "@/components/Dashboard/MainActions";
+import WelcomeSection from "@/components/Dashboard/WelcomeSection";
+import Navbar from "@/components/Navbar";
 
-const DashboardPage = () => {
+function DashboardPage() {
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <>
+      <Navbar />
 
-export default DashboardPage
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
+    </>
+  );
+}
+export default DashboardPage;
